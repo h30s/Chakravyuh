c27400f74494650b03aa2a1bd359f0feeeb5d8a9
Himanshu Soni <h30s.soni@gmail.com>
2025-05-02 13:49:00 +0530

    Fix Link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -65,7 +65,7 @@ const Header = () => {
 
         {/* Contact Button - Desktop */}
         <a
-          href="https://chakravyuh-army.lovable.app/"
+          href="https://chakravyuh-army.vercel.app/"
           className="hidden md:inline-flex btn btn-accent"
         >
           Admin Login
